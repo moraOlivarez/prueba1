bcfd649d6f73caad3b272fa1a5998e9b8720d793
mora <mora.olivarez@gmail.com>
2021-01-11 11:37:25 -0700

    finished

--- resources/views/estados.blade.php
+++ resources/views/estados.blade.php
@@ -1,14 +1,14 @@
 
 
 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
-  <link href="https://cdn.datatables.net/1.10.23/css/jquery.dataTables.min.css
-" rel="stylesheet">
+  <link href="https://cdn.datatables.net/1.10.23/css/jquery.dataTables.min.css" rel="stylesheet">
 <h1>México</h1>
 
 <div>
 
-<table id="example" class="display" style="width:1000px; height: 800px;">
+<table id="example" class="display" style="width:800px; height:800px;">
         <thead>
             <tr>
                 <th>Nombre Estado</th>
@@ -30,6 +30,9 @@
 
 
 <script src="//cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
 
 <script>
 
